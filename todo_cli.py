2785ed0d5a4224ef33f8429812180e021cf2fb6d
Kevin Gonsalves <kevgonsalves14@gmail.com>
2024-09-09 14:16:47 +0530

    todo_cli.py

--- todo_cli.py
+++ todo_cli.py
@@ -1,31 +1,63 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
 
-def convert():
-	print(output_str.set("You clicked!!!"))
+tasklist = []
 
-# Create a window
-window = tk.Tk()
-window.title("Demo")
-window.geometry("400x200")
+pg1inpt = input("---------------------------------------------------------------\n\n--> Add task (press 1)\n--> View task (press 2)\n==> ")
+print()
+print("---------------------------------------------------------------")
+print()
 
-# Title text/widget
-title_label = ttk.Label(master=window, text="Miles to Kilometers", font="Calibri 24 bold")
-title_label.pack()
+if pg1inpt == "1":
+    # Read the existing tasks to determine the starting number
+    try:
+        with open("tasks.txt", "r") as file:
+            lines = file.readlines()
+            if lines:
+                last_task = lines[-1]
+                n = int(last_task.split('.')[0])
+            else:
+                n = 0
+    except FileNotFoundError:
+        n = 0
 
-# Input field
-input_frame = tk.Frame(master=window)
-entry_int = tk.IntVar()
-usr_entry = tk.Entry(master=input_frame, textvariable=entry_int)
-button = tk.Button(master=input_frame, text="Convert", command=convert)
-usr_entry.pack(side="left", padx=10)
-button.pack(side="left")
-input_frame.pack(pady=10)
+    print("(To stop adding task press Ctrl-Z)")
+    print()
+    while True:
+        try:
+            usrtask = input("Add task: ==> ")
+            tasklist.append(usrtask)
+        except EOFError:
+            print()
+            print("---------------------------------------------------------------")
+            with open("tasks.txt", "a") as file:
+                for line in tasklist:
+                    n += 1
+                    file.write(f"{n}. {line} []\n")
+            break
 
-# Output text/widget
-output_str = tk.StringVar()
-output = ttk.Label(master=window, text="Output", font="Calibri 20", textvariable=output_str)
-output.pack()
+elif pg1inpt == "2":
+    try:
+        with open("tasks.txt", "r") as file:
+            lines = file.readlines()
+            for task in lines:
+                print(task, end="")
+            print()
+            print("---------------------------------------------------------------")
+            print()
+    except FileNotFoundError:
+        sys.exit("You haven't added a task yet :(\n\n---------------------------------------------------------------")
 
-# Run
-window.mainloop()
+    try:
+        task_number = int(input("Press task number of the task completed\n(If no task completed press ctrl-z)\n==> "))
+        with open("tasks.txt", "r") as file:
+            lines = file.readlines()
+        
+        with open("tasks.txt", "w") as file:
+            for line in lines:
+                if line.startswith(f"{task_number}."):
+                    line = line.replace("[]", "[X]")
+                file.write(line)
+        
+    except (EOFError, ValueError):
+        print()
+        sys.exit("Until next time :) ...\n---------------------------------------------------------------\n")
